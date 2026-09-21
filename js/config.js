25a9be2f0ba3a8f61d77312c4bea9750e172493f
Jefferson-Lobato <jeflobato.uni@gmailcom>
2026-09-21 13:59:32 -0300

    configuraçao url projeto committ

--- js/config.js
+++ js/config.js
@@ -1,3 +1,3 @@
 // Cole aqui os dados do seu projeto Supabase.
-window.SUPABASE_URL = "https://SEU-PROJETO.supabase.co";
-window.SUPABASE_ANON_KEY = "SUA_ANON_KEY";
+window.SUPABASE_URL = "https://kbuffaedveflhnrbndzg.supabase.co";
+window.SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtidWZmYWVkdmVmbGhucmJuZHpnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODk5OTc5MjQsImV4cCI6MjEwNTU3MzkyNH0.55PYN-QvDsKFtXVXTyeGWcKZAys7Z3ffpZDi8C4Xl-w";
